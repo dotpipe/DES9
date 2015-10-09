aa08cffbf6ec99d1b127638f2c896850da747e8a
Anthony David Pulse, Jr. <4723286.ap@gmail.com>
2015-10-09 13:51:12 -0400

    Update kickdeck.cpp

--- kickdeck.cpp
+++ kickdeck.cpp
@@ -9,77 +9,84 @@
 #include <sstream>
 #include <cstring>
 #include <vector>
+#include <iterator>
+#include <exception>
 
 using namespace std;
 
-std::vector<bool> dmiro(long n_bytes, std::vector<bool> tmp) {
-
-    int y;
-
+std::vector<int> dmiro(long n_bytes, std::vector<int> tmp) {
+    
+std::cout << n_bytes;
+    int *e;
     if (tmp.size()>=n_bytes*8+1)
         return tmp; 
     else
-        tmp.push_back(std::vector<bool>);
-
-    for (std::vector<bool>::iterator v : tmp) {
-        std::cout << v;
-       
-
-       if (strcmp((const char*)y,"00"))
-              tmp.insert(0,v>>4);
-       if (strcmp((const char*)y,"10"))
-              tmp.insert(0,v>>3);
-       if (strcmp((const char*)y,"11"))
-              tmp.insert(0,v<<1);
-       if (strcmp((const char*)y,"01"))
-              tmp.insert(0,v>>2);
-        y=tmp.at(tmp.end()-v-1) + tmp.at(tmp.end()-v);
+        tmp.push_back(std::vector<int>);
+    
+    for (int v : tmp) {
+        try {
+            if (strcmp((const int*)y,"00"))
+              tmp.push_back(v>>4);
+            if (strcmp((const int*)y,"10"))
+              tmp.push_back(v>>3);
+            if (strcmp((const int*)y,"11"))
+              tmp.push_back(v<<1);
+            if (strcmp((const int*)y,"01"))
+              tmp.push_back(v>>2);
+              int *y[3]= { tmp[v-1], tmp[v], '\0' };
+        }
+        catch (std::exception &e) {
+            tmp.reserve(n_bytes*8+1);
+            throw e;
+        }
     }
-
     return dmiro( n_bytes, tmp);
 }
 
-std::vector<bool> mirror(std::vector<bool> tmp) {
-
-    int y;
+std::vector<int> mirror(std::vector<int> tmp) {
 
     if (sizeof(tmp)<=5)
         return tmp;
 
-    for (auto v : tmp) {
+    for (int v=tmp.end();v<=tmp.begin();--v) {
+        try {
 
-       if (strcmp((const char*)y,"00"))
-              tmp.insert(0,v<<4);
-       if (strcmp((const char*)y,"01"))
-              tmp.insert(0,v<<2);
-       if (strcmp((const char*)y,"10"))
-              tmp.insert(0,v<<3);
-       if (strcmp((const char*)y,"11"))
-              tmp.insert(0,v>>1);
+       if (strcmp((const int*)y,"00"))
+              tmp.push_back(v<<4);
+       if (strcmp((const int*)y,"01"))
+              tmp.push_back(v<<2);
+       if (strcmp((const int*)y,"10"))
+              tmp.push_back(v<<3);
+       if (strcmp((const int*)y,"11"))
+              tmp.push_back(v>>1);
        else tmp.pop_back();
-       
-       y=tmp.at(tmp.end()-v-1) + tmp.at(tmp.end()-v);
+        bool *y[3]={ tmp[v-1], tmp[v], '\0' };
+        }
+        
+        //catch
+        
     }
     return mirror(tmp);
 }
 
 int main(int x, char ** argc, char * argv[]) {
+    int *e;
     fstream out;
-    out.open (argv[4], ios::out | ios::binary);
+    out.open (argv[2], ios::out | ios::binary);
     fstream in;
-    in.open (argv[3], ios::in | ios::binary);
+    in.open (argv[1], ios::in | ios::binary);
     in.seekg (0, in.end);
-    std::ostringstream length;
+    std::istringstream length;
     length << in.tellg();
-    int lenint=std::stol(length.str());
-    in.seekg (0, in.beg);b
+    long lenint=std::stol(length.str());
+    in.seekg (0, in.beg);
     if (strcmp("-c",argv[0]) && in.is_open() && out.is_open()) {
-        std::vector<bool> tmp;
+        std::vector<int> tmp;
         if (tmp.max_size()>=lenint) {}
         else return 0;
-        char * tmpvar= new char[sizeof(char)*lenint];
-        tmp.reserve(sizeof(tmpvar));
-        in.read(tmpvar,lenint);
+        int * tmpvar= new int[sizeof(int)*lenint*8+1];
+        tmp.reserve(sizeof(tmpvar)+1);
+        in.read(tmpvar,length);
         
         for (int v=0;v<=sizeof(tmpvar);v++)
             tmp.push_back(tmpvar[v]);
@@ -91,18 +98,24 @@ int main(int x, char ** argc, char * argv[]) {
         out << endl;
     }
     if (strcmp("-d",argv[0])) {
+        in.close();
         std::cout << "enter 5 bits";
-        char argy[6]={ };
+        int argy;
         std::cin >> argy;
         std::cout << "enter byte size";
         std::cin >> lenint;
-        std::vector<bool> tmp;
-        tmp.reserve(30000000);
-        if (lenint*8+1>=tmp.max_size())
+        try {
+        std::vector<int> tmp (6,0);
+        tmp.reserve(30000);
+        if (lenint*8+1<=tmp.max_size())
             return 0;
-        else
-            tmp.insert(tmp.begin(),argy,argy+5);
-        std::cout << "Let's do this!" << tmp << endl;
+        tmp[0].push_back(argy);
+        }
+        catch (std::exception &e) {
+            tmp.reserve(100000);
+            throw e;
+        }
+        std::cout << "Let's do this!" << endl;
 
         tmp=dmiro(lenint,tmp);
     
